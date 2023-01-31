e6d0e146a22ef6ef9d0276e3eb6104127971cd37
andres06-hub <andressierrarojas7@gmail.com>
2023-01-31 17:55:33 -0500

    ADD: implement ResponseSaleDto

--- src/main/java/com/konex/medicines/sales/SaleController.java
+++ src/main/java/com/konex/medicines/sales/SaleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.konex.medicines.common.dto.ResponseDto;
+import com.konex.medicines.common.dto.ResponseSaleDto;
 import com.konex.medicines.common.dto.ReturnTypeDto;
 import com.konex.medicines.products.ProdEntity;
 import com.konex.medicines.products.ProdService;
@@ -30,12 +31,12 @@ public class SaleController {
   SaleService _saleSrv;
 
   @PostMapping("sale")
-  public ResponseEntity<ResponseDto> createSale(@RequestBody SaleEntity data) {
+  public ResponseEntity<ResponseSaleDto> createSale(@RequestBody SaleEntity data) {
       // validate if the product has sufficient quantity
       ProdEntity medicine = this._prodSrv.findByPublicId(data.publicIdMedicine);
-      if (medicine == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDto("The drug does not exist"));
-      if (medicine.getQuantityStock() <= 0) return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("There is no products!"));
-      if (medicine.getQuantityStock() < data.getQuantity()) return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("Medicine '"+ data.getName() + "', does not have enough products: Current stock: " + medicine.getQuantityStock()));
+      if (medicine == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseSaleDto("The drug does not exist"));
+      if (medicine.getQuantityStock() <= 0) return ResponseEntity.status(HttpStatus.OK).body(new ResponseSaleDto("There is no products!"));
+      if (medicine.getQuantityStock() < data.getQuantity()) return ResponseEntity.status(HttpStatus.OK).body(new ResponseSaleDto("Medicine '"+ data.getName() + "', does not have enough products: Current stock: " + medicine.getQuantityStock()));
       Integer resultStock = medicine.getQuantityStock() - data.getQuantity();
       //Update product Stock
       medicine.setQuantityStock(resultStock);
@@ -44,23 +45,28 @@ public class SaleController {
         result = this._prodSrv.updateProd(medicine.getId(), medicine);
       } catch (Exception e) {
         log.error("ERROR:" + e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto("ERROR in DB: "+e));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseSaleDto("ERROR in DB: "+e));
       }
       if (result.getStatus()) log.info("Updated Medicine: " + result.getData());
-      //Save Sale
-      data.setTotalValue(data.getQuantity()*data.getUnitPrice());
+      // Round value
+      double totalValue = data.getQuantity()*data.getUnitPrice();
+      double finalValue = Math.round( totalValue * 100.0 ) / 100.0;
+      data.setTotalValue(finalValue);
+      //Generate DateTime 
       String dataTime = this._saleSrv.generateDateTime();
       data.setSaleDateTime(dataTime);
       SaleEntity sale;
+      //Save Sale
       try {
         sale = this._saleSrv.save(data);
       } catch (Exception e) {
         log.error("ERROR: " + e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto("ERROR in DB: ", e));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseSaleDto("ERROR in DB: " + e));
       }
       log.info("SAVE sale: " + sale);
-      //TODO: crear clase para la respuesta de la compra, los datos de la compara
-      return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("Purchase successfully created!"));
+      return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(new ResponseSaleDto("Purchase successfully created!",sale.getId(),sale.getPublicIdMedicine(), sale.getName(), sale.getQuantity(), sale.getTotalValue()));
   }
 
   //Filtrar
