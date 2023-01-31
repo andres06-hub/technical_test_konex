d46dd354ef4f559232ca1d779dfaec3bcaeadb95
andres06-hub <andressierrarojas7@gmail.com>
2023-01-31 16:19:41 -0500

    ADD: Import config Redis

--- src/main/java/com/konex/medicines/MedicinesApplication.java
+++ src/main/java/com/konex/medicines/MedicinesApplication.java
@@ -2,10 +2,13 @@ package com.konex.medicines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.konex.medicines.config.RedisConfig;
 
 @SpringBootApplication
+@Import(RedisConfig.class)
 public class MedicinesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MedicinesApplication.class, args);
 	}
